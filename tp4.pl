24c3f20d824e6a9e99f5aa44ad31b4a2390104e0
spapillon <papillonsimon@gmail.com>
2013-01-11 15:38:31 -0500

    Minor fixes

--- tp4.pl
+++ tp4.pl
@@ -1,15 +1,15 @@
 % partie syntaxique:
-ph(p(GroupNom, GroupeVerbe)) --> group_nom(GroupeNom, Nombre,Genre), groupe_verbe(GroupeVerb,Nombre).
+ph(p(GroupeNom, GroupeVerbe)) --> groupe_nom(GroupeNom, Nombre, _), groupe_verbe(GroupeVerbe, Nombre).
 groupe_nom(gn(Determinant, Nom), Nombre, Genre) --> determinant(Determinant, Nombre, Genre), nom(Nom, Nombre, Genre).
 groupe_nom(gn(Nom_Propre), sing, Genre) --> nomp(Nom_Propre, Genre).
-groupe_verbe(gv(Verbe,GroupeNom), Nombre) --> verbe(Verbe, Nombre), groupe_nom(GroupeNom, _, _).
+groupe_verbe(gv(Verbe, GroupeNom), Nombre) --> verbe(Verbe, Nombre), groupe_nom(GroupeNom, _, _).
 groupe_verbe(gv(Verbe), Nombre) --> verbe(Verbe, Nombre).
 
 % partie lexicale:
 determinant(d(Determinant), Nombre, Genre) --> [Determinant], {est_det(Determinant, Nombre, Genre)}.
 nom(n(Nom), Nombre, Genre) --> [Nom], {est_nom(Nom, Nombre, Genre)}.
 nomp(np(NomPropre), Genre) --> [NomPropre], {est_np(NomPropre, Genre)}.
-verbe(v(Verbe), Nombre) --> [Verbe], {est_verbe(Verbe, Nom)}.
+verbe(v(Verbe), Nombre) --> [Verbe], {est_verbe(Verbe, Nombre)}.
 
 
 
@@ -17,10 +17,11 @@ verbe(v(Verbe), Nombre) --> [Verbe], {est_verbe(Verbe, Nom)}.
 %                    Dictionnaire
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+est_np(Nom, sing, Genre) :- personne(Nom, Genre).
 
-personne(jean).
-personne(marie).
-personne(claude).
+personne(jean, masc).
+personne(marie, fem).
+personne(claude, masc).
 
 nom(fruit, comestible) :- nom(pomme, comestible).
 nom(fruit, comestible) :- nom(orange, comestible).
