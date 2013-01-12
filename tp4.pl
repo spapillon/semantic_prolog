25ca9087dd661e5f99132aabd3b39851da2cec04
spapillon <papillonsimon@gmail.com>
2013-01-11 19:55:26 -0500

    Changed structure of the program, now infers semantics

--- tp4.pl
+++ tp4.pl
@@ -1,91 +1,89 @@
 % partie syntaxique:
-ph(p(GroupeNom, GroupeVerbe)) -->
-  groupe_nom(GroupeNom, Nombre, _, ClasseAgent),
-  groupe_verbe(GroupeVerbe, Nombre, ClasseAgent).
+ph(semantique(SemantiqueNom, SemantiqueVerbe)) -->
+  groupe_nom(Nombre, _, SemantiqueNom),
+  groupe_verbe(Nombre, SemantiqueNom, SemantiqueVerbe).
 
-groupe_nom(gn(Determinant, Nom), Nombre, Genre, Classe) -->
-  determinant(Determinant, Nombre, Genre),
-  nom(Nom, Nombre, Genre, Classe).
-groupe_nom(gn(Nom_Propre), sing, Genre, anime) -->
-  nomp(Nom_Propre, Genre).
-groupe_nom(gn(GroupeNom1, Preposition, Nom), Nombre, Genre, Classe) -->
-  groupe_nom(GroupeNom1, Nombre, Genre, Classe),
-  preposition(Preposition, PrepNombre),
-  nom(Nom, PrepNombre, _, _).
-groupe_nom(gn(GroupeNom1, Preposition, Nom), Nombre, Genre, Classe) -->
-  groupe_nom(GroupeNom1, Nombre, Genre, Classe),
-  preposition(Preposition, _),
-  nomp(Nom, _).
+groupe_nom(Nombre, Genre, SemantiqueNom) -->
+  determinant(Nombre, Genre),
+  nom(Nombre, Genre, SemantiqueNom).
+groupe_nom(sing, Genre, SemantiqueNom) -->
+  nomp(Genre, SemantiqueNom).
 
-groupe_verbe(gv(Verbe), Nombre, ClasseAgent) -->
-  verbe_intransitif(Verbe, Nombre, ClasseAgent).
-groupe_verbe(gv(Verbe, GroupeNom), Nombre, ClasseAgent) -->
-  verbe_transitif(Verbe, Nombre, ClasseAgent, ClasseObjet),
-  groupe_nom(GroupeNom, _, _, ClasseObjet).
+groupe_verbe(Nombre, Agent, SemantiqueVerbe) -->
+  verbe_intransitif(Nombre, Agent, SemantiqueVerbe).
+groupe_verbe(Nombre, Agent, SemantiqueVerbe) -->
+  verbe_transitif(Nombre, Agent, Objet, SemantiqueVerbe),
+  groupe_nom(_, _, Objet).
 
 % partie lexicale:
-determinant(d(Determinant), Nombre, Genre) -->
+determinant(Nombre, Genre) -->
   [Determinant],
   {est_det(Determinant, Nombre, Genre)}.
-nom(n(Nom), Nombre, Genre, Classe) --> 
+nom(Nombre, Genre, SemantiqueNom) --> 
   [Nom],
-  {est_nom(Nom, Nombre, Genre, Classe)}.
-nomp(np(NomPropre), Genre) -->
+  {est_nom(Nom, Nombre, Genre, SemantiqueNom)}.
+nomp(Genre, SemantiqueNomPropre) -->
   [NomPropre],
-  {est_np(NomPropre, Genre)}.
-verbe_intransitif(v(Verbe), Nombre, ClasseAgent) --> 
+  {est_np(NomPropre, Genre, SemantiqueNomPropre)}.
+verbe_intransitif(Nombre, Agent, SemantiqueVerbe) --> 
   [Verbe],
-  {est_verbe_intransitif(Verbe, Nombre, ClasseAgent)}.
-verbe_transitif(v(Verbe), Nombre, ClasseAgent, ClasseObjet) --> 
+  {est_verbe_intransitif(Verbe, Nombre, Agent, SemantiqueVerbe)}.
+verbe_transitif(Nombre, Agent, Objet, SemantiqueVerbe) --> 
   [Verbe],
-  {est_verbe_transitif(Verbe, Nombre, ClasseAgent, ClasseObjet)}.
-preposition(prep(Preposition), Nombre) -->
+  {est_verbe_transitif(Verbe, Nombre, Agent, Objet, SemantiqueVerbe)}.
+preposition(Nombre, Agent, Objet, SemantiquePrep) -->
   [Preposition],
-  {est_preposition(Preposition, Nombre)}.
+  {est_preposition(Preposition, Nombre, Agent, Objet, SemantiquePrep)}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                    Dictionnaire
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+anime(jean).
+comestible(pomme).
 
-est_np(Nom, Genre) :-
-  personne(Nom, Genre).
-personne(jean, masc).
-personne(marie, fem).
-personne(claude, masc).
+est_np(jean, masc, jean).
+est_np(marie, fem, marie).
+est_np(claude, masc, claude).
 
-est_nom(Nom, Nombre, Genre, Classe) :-
-  nom(Nom, Nombre, Genre, Classe).
-nom(fruit, sing, masc, comestible).
-nom(pomme, sing, fem, comestible).
-nom(orange, sing, fem, comestible).
-nom(jus,sing, masc, comestible).
-nom(personne, sing, fem, anime).
-nom(etudiant, sing, masc, anime).
-nom(machine, sing, fem, _).
-nom(chanson, sing, fem, rien).
+est_nom(fruit, sing, masc, fruit).
+est_nom(fruits, plur, masc, fruit).
+est_nom(pomme, sing, fem, pomme).
+est_nom(pommes, plur, fem, pomme).
+est_nom(orange, sing, fem, orange).
+est_nom(oranges, plur, fem, orange).
+est_nom(jus, sing, _, jus).
+est_nom(personne, sing, fem, personne).
+est_nom(personnes, plur, fem, personne).
+est_nom(etudiant, sing, masc, etudiant).
+est_nom(etudiants, plur, masc, etudiant).
+est_nom(machine, sing, fem, machine).
+est_nom(machines, plur, fem, machine).
+est_nom(chanson, sing, fem, chason).
+est_nom(chansons, plur, fem, chason).
 
-est_verbe_transitif(Verbe, Nombre, ClasseAgent, ClasseObjet) :-
-  verbe_transitif(Verbe, Nombre, ClasseAgent, ClasseObjet).
-verbe_transitif(demande, sing, anime, _).
-verbe_transitif(donne, sing, anime, _).
-verbe_transitif(mange, sing, anime, comestible).
-verbe_transitif(marche, sing, anime, _).
+est_verbe_transitif(demande, sing, X, Y, demander(X, Y)).
+est_verbe_transitif(demandent, plur, X, Y, demander(X, Y)).
+est_verbe_transitif(mange, sing, X, Y, manger(X,Y)) :-
+  anime(X),
+  comestible(Y).
+est_verbe_transitif(mangent, plur, X, Y, manger(X,Y)) :-
+  anime(X),
+  comestible(Y).
+est_verbe_transitif(aiment, plur, X, Y, aimer(X,Y)) :-
+  anime(X).
+est_verbe_transitif(aime, sing, X, Y, aimer(X,Y)) :-
+  anime(X).
+est_verbe_intransitif(mange, sing, X, manger(X)) :-
+  anime(X).
+est_verbe_intransitif(mangent, plur, X, manger(X)) :-
+  anime(X).
 
-est_verbe_intransitif(Verbe, Nombre, ClasseAgent) :-
-  verbe_intransitif(Verbe, Nombre, ClasseAgent).
-verbe_intransitif(marche, sing, anime).
-verbe_intransitif(chante, sing, anime).
+est_det(un, sing, masc).
+est_det(une, sing, fem).
+est_det(des, plur, _).
+est_det(les, plur, _).
 
-
-est_det(Determinant, Genre, Nombre) :-
-  det(Determinant, Genre, Nombre).
-det(un, sing, masc).
-det(une, sing, fem).
-det(des, plur, _).
-det(les, plur, _).
-
-est_preposition(Preposition, Nombre) :-
-  preposition(Preposition, Nombre).
-preposition(a, sing).
-preposition(de, sing).
-preposition(des, plur).
+est_preposition(a, sing, X, Y, receveur(X, Y)).
+est_preposition(aux, plus, X, Y, receveur(X, Y)).
+est_preposition(de, sing, X, Y, constituant(X, Y)).
+est_preposition(des, plur, X, Y, constituant(X, Y)).
